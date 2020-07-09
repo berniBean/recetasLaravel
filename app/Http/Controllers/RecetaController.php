a49a63f2972e41d304a7f52b3cdc090367cdf15d
berniBean <ernesto_fope55@hotmail.com>
2020-07-09 12:18:45 -0500

    agregando los archivos nuevos de seed

---
+++ app/Http/Controllers/RecetaController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Receta;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class RecetaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        return view('recetas.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('recetas.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $data = $request ->validate([
+            'titulo'=>'required|min:6'
+        ]);
+
+        DB::table('recetas')->insert([
+            'titulo'=>$data['titulo']
+        ]);
+        
+        //redireccionar
+        return redirect()->action('RecetaController@index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Receta $receta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Receta  $receta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Receta $receta)
+    {
+        //
+    }
+}
